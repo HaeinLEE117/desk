f7df1548276e97c91bf37a0c9dc14fcd3620e70d
HaeinLEE <heyin0122@korea.ac.kr>
2021-11-05 17:15:42 +0900

    회피2번함수까지정의

--- process_3.php
+++ process_3.php
@@ -22,116 +22,83 @@ $PointNumber = $_GET['PointNumber'];  //$PointNumber
 $Destination = $_GET['Destination'];  //$Destination
 $Product = $_GET['Product'];  //$Product
 
-//(1)
 
-is_setted_route($VehicleNumber, $PointNumber);
-
-
-
-
-
-//-------------------------------------------------이전 프로그램--------------------------------------------------------------------------//
-
-    //readed RFID가 특정 위치일 때 set_collision 호출 (출발지에서 함수를 호출함 )
-    $start_point = array(101,406,306,2307,307,2101);
-    $set_collision_flag = 0;
-    $combine_PN_DN = "";
-
-    for($i = 0; $i<6; $i++){
-        if($PointNumber == $start_point[$i]){
-            $set_collision_flag = 1;
-        }
+//---------------------------------MAIN()-----------------------------------
+if($PointNumber == $Destination){
+    driving_end($VehicleNumber); //(0)
+}else{
+    if(is_setted_route($VehicleNumber, $PointNumber)){ //(1)
+        tracking_route($VehicleNumber, $PointNumber); //(2)-2
+    }else{ //기존에 입력된 경로가 없는 경우 목적지 설정 후 주행. 출발지, 목적지 오류는 무시
+        check_start_point($PointNumber); //(2)-1
+        set_collision($VehicleNumber, $PointNumber, $Destination); //(3)
     }
+}
 
-    //정상적으로 출발지에서 하는 경우
-
-
-
-
-        $query ="insert into agvlocation".$VehicleNumber."(Direction, PointNumber, Destination, Time)
-        VALUES(1,                  
-        $PointNumber,
-        $Destination,
-        now()
-        )
-        ";
-
-
-
-          
-
-
-    //---------충돌 방지 코드 쓰기
-
-//enqueue 여기서 하기 (함수 리턴값ㄷ으로)
-// 입력 형식 echo $agv_route[$VehicleNumber][0][0] => 출럭 "306";
 
 mysqli_close($mysqli); 
-
-
 
 //=====================================================
 //                      함수
 //=====================================================
 
+function driving_end($VN){//(0) .주행 완료 판단 후 경로 0 설정
+    //point부분 0으로 설정하는 거 추가 할 것 -------------------------------------------------------------------------
+    $mysqli = new mysqli('localhost', 'root', '1234', 'agv_monitor'); 
+    $query = "update agvs_info set route='0' where VehicleNumber = ".$VN;
+    $result = mysqli_query($mysqli,$query);
+
+}
+
 
 function is_setted_route($VN, $PN){ //(1)기존에 설정된 경로가 있는지 check
 $mysqli = new mysqli('localhost', 'root', '1234', 'agv_monitor'); 
+$query = "select route from agvs_info WHERE VehicleNumber = ".$VN;
+$result = mysqli_query($mysqli,$query);
 
-    $query = "select route from agvs_info WHERE VehicleNumber = ".$VN;
-    echo $query;
-    $result = mysqli_query($mysqli,$query);
     if($result){
-        echo "resulteed</br>";
         $row = mysqli_fetch_array($result);
         if($row[0]==NULL){ //NULL인 경우 여기 출력 VN조회 오류 => error_log tale 기록
-            echo "null find";
+            $result_2 = mysqli_query($mysqli,"insert into error_log(code,message,time) values('1101','VN_no_data',now())");
+            if($result_2){
             return false;
+            }else{
+                echo "SQL ERROR";
+            }
         }elseif($row[0]){ //=> 경로 따라가는 함수 실행
-            echo "경로설정됨";
             return true;
         }else{ //0으로 설정된 경우 여기 출력 => 경로 새로 설정
-            echo "find nothing";
             return false;
         }
-        /*
-        if($row[0]){
-            echo $row[0];
-            return true;
-        }else{
-            echo "no data";
-            return false;
-        }
-    }else{
-        echo "insert into error_log(code,message,time) values('1101','VN_no_data',now())";
-        return false;
     }
-    */
-}
 }
 
 
 function check_start_point($PN){//(2)-1 정상적으로 출발지에서 출발하는지
+    $mysqli = new mysqli('localhost', 'root', '1234', 'agv_monitor'); 
+    echo "새로운 경로를 설정합니다".$PN;
     $start_point = array(101,406,306,2307,307,2101);
-    $flag = 0;
+    $flag = 0;2
     for($i = 0; $i<6; $i++){
         if($start_point[$i]==$PN){
             $flag = 1;
         }
+    }
+    if(!$flag){
+        $result_2 = mysqli_query($mysqli,"insert into error_log(code,message,time) values('1102','start_point_error',now())");
+        return false;
     }
     return flag;
 }
 
 
 function tracking_route($VN, $PN){//(2)-2 경로 따라가는 함수 작성
-
+    echo "기존 경로를 따라갑니다.";
 }
 
-function set_collision($called_destination, $readed_RFID){   //목적지 한쌍이 정상적으로 입력되었을 때 충돌이 예상되는 location point를 점령함 
+function set_collision($VN, $PN, $DN){   //목적지 한쌍이 정상적으로 입력되었을 때 충돌이 예상되는 location point를 점령함 
     $dic_destination_positiont = array('101'=>'406', '306'=>'2307', '307'=>'2101',
     '406'=>'101', '2307'=>'306','2101'=>'307');
-
-
 
     if($readed_RFID == $dic_destination_positiont[$called_destination]){
 
